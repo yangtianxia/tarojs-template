aa2af6ad5381126baeb4df69f75fde3cf080a82d
yangtianxia <1304082170@qq.com>
2023-11-08 10:24:31 +0800

    chore: add button component

---
+++ src/components/button/types.ts
@@ -0,0 +1,5 @@
+export type ButtonSize = 'normal' | 'large' | 'small' | 'mini'
+
+export type ButtonType = 'default' | 'primary' | 'danger' | 'warning' | 'success' | 'cell'
+
+export type ButtonIconPosition = 'left' | 'right'
