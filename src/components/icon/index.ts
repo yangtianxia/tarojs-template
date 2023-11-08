0203223afe1b127958c3126d4fefc8c92f8dfddd
yangtianxia <1304082170@qq.com>
2023-11-08 10:28:42 +0800

    chore: add icon component

---
+++ src/components/icon/index.ts
@@ -0,0 +1,10 @@
+import { withInstall } from '../utils/with-install'
+import _Icon from './Icon'
+
+import './index.less'
+
+export const Icon = withInstall(_Icon)
+export default Icon
+
+export type { IconProps } from './Icon'
+export type { IconName } from './types'
