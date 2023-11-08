91e7d6a317894da695702043b82844563d69dc97
yangtianxia <1304082170@qq.com>
2023-11-08 10:25:32 +0800

    chore: add checkbox component

---
+++ src/components/checkbox/Group.tsx
@@ -0,0 +1,78 @@
+import type { CheckerDirection } from '../checkbox/Checker'
+
+import BEM from '@/shared/bem'
+import { defineComponent, watch, type PropType, type InjectionKey, type ExtractPropTypes } from 'vue'
+import { isBoolean } from '@txjs/bool'
+
+import { useExpose } from '../composables/expose'
+import { useFieldValue } from '../composables/field-value'
+import { useChildren } from '../composables/children'
+import { numericProp, makeArrayProp } from '../utils'
+
+import type { CheckboxGroupProvide, CheckboxGroupToggleAllOptions } from './types'
+
+const [name, bem] = BEM('checkbox-group')
+
+export const checkboxGroupProps = {
+  max: numericProp,
+  disabled: Boolean,
+  iconSize: numericProp,
+  checkedColor: String,
+  direction: String as PropType<CheckerDirection>,
+  value: makeArrayProp<unknown>(),
+  onChange: Function as PropType<(value: unknown[]) => void>,
+  'onUpdate:value': Function as PropType<(value: unknown[]) => void>
+}
+
+export type CheckboxGroupProps = ExtractPropTypes<typeof checkboxGroupProps>
+
+export const CHECKBOX_GROUP_KEY: InjectionKey<CheckboxGroupProvide> = Symbol(name)
+
+export default defineComponent({
+  name,
+
+  props: checkboxGroupProps,
+
+  setup(props, { emit, slots }) {
+    const { children, linkChildren } = useChildren(CHECKBOX_GROUP_KEY)
+
+    const updateValue = (value: unknown[]) => emit('update:value', value)
+
+    const toggleAll = (options: CheckboxGroupToggleAllOptions = {}) => {
+      if (isBoolean(options)) {
+        options = { checked: options }
+      }
+
+      const { checked, skipDisabled } = options
+      const checkedChildren = children.filter((item) => {
+        if (!item.props.bindGroup) {
+          return false
+        }
+        if (item.props.disabled && skipDisabled) {
+          return item.checked.value
+        }
+        return checked ?? !item.checked.value
+      })
+
+      const names = checkedChildren.map((item) => item.name)
+      updateValue(names)
+    }
+
+    watch(
+      () => props.value,
+      (value) => props.onChange?.(value)
+    )
+
+    useExpose({ toggleAll })
+
+    useFieldValue(() => props.value)
+
+    linkChildren({ props, updateValue })
+
+    return () => (
+      <view class={bem([props.direction])}>
+        {slots.default?.()}
+      </view>
+    )
+  }
+})
---
+++ src/components/checkbox/types.ts
@@ -0,0 +1,36 @@
+import type { CheckboxProps } from './Checkbox'
+import type { CheckboxGroupProps } from './Group'
+
+import type { ComponentPublicInstance, ComputedRef } from 'vue'
+import type { CheckerParent, CheckerShape, CheckerDirection, CheckerLabelPosition } from './Checker'
+
+export type CheckboxGroupDirection = CheckerDirection
+export type CheckboxGroupToggleAllOptions =
+  | boolean
+  | {
+    checked?: boolean
+    skipDisabled?: boolean
+  }
+
+export type CheckboxGroupExpose = {
+  toggleAll: (options?: CheckboxGroupToggleAllOptions) => void
+}
+
+export type CheckboxGroupProvide = CheckerParent & {
+  props: CheckboxGroupProps
+  updateValue: (value: unknown[]) => void
+}
+
+export type CheckboxShape = CheckerShape
+export type CheckboxLabelPosition = CheckerLabelPosition
+
+export type CheckboxExpose = {
+  toggle: (newValue?: boolean) => void
+  /** @private */
+  props: CheckboxProps
+  /** @private */
+  checked: ComputedRef<boolean>
+}
+
+export type CheckboxGroupInstance = ComponentPublicInstance<CheckboxGroupProps, CheckboxGroupExpose>
+export type CheckboxInstance = ComponentPublicInstance<CheckboxProps, CheckboxExpose>
