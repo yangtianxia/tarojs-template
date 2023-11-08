ee0874c6f737787af64a03a4b27b89972714743a
yangtianxia <1304082170@qq.com>
2023-11-08 11:26:14 +0800

    chore: add main package home page

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,18 @@
+import BEM from '@/shared/bem'
+import { defineComponent } from 'vue'
+
+import less from './index.module.less'
+
+const [name] = BEM('home', less)
+
+export default defineComponent({
+  name,
+
+  setup() {
+    return () => (
+      <gm-app>
+        <gm-body></gm-body>
+      </gm-app>
+    )
+  }
+})
