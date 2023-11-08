6ec2e7862621c2f0ed2468f456ec0889cb289b6c
yangtianxia <1304082170@qq.com>
2023-11-08 09:28:02 +0800

    chore: add project compile mode configuration

---
+++ compile.config.js
@@ -0,0 +1,23 @@
+module.exports = [
+  {
+    title: '首页',
+    page: 'pages/home/index',
+    query: '',
+    launchMode: 'default',
+    scene: null
+  },
+  {
+    title: '登录页面',
+    page: 'subpackages/login/index',
+    query: '',
+    launchMode: 'default',
+    scene: null
+  },
+  {
+    title: '错误页面',
+    page: 'subpackages/error/index',
+    query: '',
+    launchMode: 'default',
+    scene: null
+  }
+]
