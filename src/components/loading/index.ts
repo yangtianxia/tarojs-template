e572217d8e1a41e1c853cdd2d9a1aa46f15d5e38
yangtianxia <1304082170@qq.com>
2023-11-08 10:29:22 +0800

    chore: add loading component

---
+++ src/components/loading/index.ts
@@ -0,0 +1,9 @@
+import { withInstall } from '../utils/with-install'
+import _Loading from './Loading'
+
+import './index.less'
+
+export const Loading = withInstall(_Loading)
+export default Loading
+
+export type { LoadingProps } from './Loading'
