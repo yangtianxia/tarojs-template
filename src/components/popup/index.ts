244706fe11c046ba408006afc8f2695580f116d9
yangtianxia <1304082170@qq.com>
2023-11-13 17:32:48 +0800

    chore: add popup sub component popupContent and popupFooter

--- src/components/popup/index.ts
+++ src/components/popup/index.ts
@@ -1,9 +1,24 @@
+import type { App } from 'vue'
 import { withInstall } from '../utils/with-install'
-import _Popup from './Popup'
+import _Popup, { Content as _Content, Footer as _Footer } from './Popup'
 
 import './index.less'
 
-export const Popup = withInstall(_Popup)
+export const PopupContent = withInstall(_Content)
+export const PopupFooter = withInstall(_Footer)
+export const Popup = withInstall(_Popup, {
+  Content: PopupContent,
+  Footer: PopupFooter
+})
+
+const popupInstall = Popup.install
+
+Popup.install = (app: App) => {
+  PopupContent.install(app)
+  PopupFooter.install(app)
+  popupInstall(app)
+}
+
 export default Popup
 
 export { popupProps, POPUP_KEY } from './Popup'
