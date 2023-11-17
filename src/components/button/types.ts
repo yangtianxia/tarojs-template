1aad06aab9bdb19a28ab6c68896cccd692651ccc
yangtianxia <1304082170@qq.com>
2023-11-17 14:24:19 +0800

    chore(component): 优化button组件样式

--- src/components/button/types.ts
+++ src/components/button/types.ts
@@ -1,5 +1,5 @@
 export type ButtonSize = 'normal' | 'large' | 'small' | 'mini'
 
-export type ButtonType = 'default' | 'primary' | 'danger' | 'warning' | 'success' | 'cell'
+export type ButtonType = 'default' | 'light' | 'primary' | 'danger' | 'warning' | 'success' | 'cell'
 
 export type ButtonIconPosition = 'left' | 'right'
