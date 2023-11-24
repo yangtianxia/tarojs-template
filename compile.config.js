ec6593da00c21c75bf552ab92513e068d193fff3
yangtianxia <1304082170@qq.com>
2023-11-24 15:19:33 +0800

    chore: 配置examples环境

--- compile.config.js
+++ compile.config.js
@@ -25,111 +25,127 @@ module.exports = [
     page: 'examples/button/index',
     query: '',
     launchMode: 'default',
-    scene: null
+    scene: null,
+    mode: ['examples']
   },
   {
     title: 'cascader-示例',
     page: 'examples/cascader/index',
     query: '',
     launchMode: 'default',
-    scene: null
+    scene: null,
+    mode: ['examples']
   },
   {
     title: 'grid-示例',
     page: 'examples/grid/index',
     query: '',
     launchMode: 'default',
-    scene: null
+    scene: null,
+    mode: ['examples']
   },
   {
     title: 'cell-示例',
     page: 'examples/cell/index',
     query: '',
     launchMode: 'default',
-    scene: null
+    scene: null,
+    mode: ['examples']
   },
   {
     title: 'checkbox-示例',
     page: 'examples/checkbox/index',
     query: '',
     launchMode: 'default',
-    scene: null
+    scene: null,
+    mode: ['examples']
   },
   {
     title: 'radio-示例',
     page: 'examples/radio/index',
     query: '',
     launchMode: 'default',
-    scene: null
+    scene: null,
+    mode: ['examples']
   },
   {
     title: 'result-示例',
     page: 'examples/result/index',
     query: '',
     launchMode: 'default',
-    scene: null
+    scene: null,
+    mode: ['examples']
   },
   {
     title: 'form-示例',
     page: 'examples/form/index',
     query: '',
     launchMode: 'default',
-    scene: null
+    scene: null,
+    mode: ['examples']
   },
   {
     title: 'alert-示例',
     page: 'examples/alert/index',
     query: '',
     launchMode: 'default',
-    scene: null
+    scene: null,
+    mode: ['examples']
   },
   {
     title: 'list-示例',
     page: 'examples/list/index',
     query: '',
     launchMode: 'default',
-    scene: null
+    scene: null,
+    mode: ['examples']
   },
   {
     title: 'footer-bar-示例',
     page: 'examples/footer-bar/index',
     query: '',
     launchMode: 'default',
-    scene: null
+    scene: null,
+    mode: ['examples']
   },
   {
     title: 'action-sheet-示例',
     page: 'examples/action-sheet/index',
     query: '',
     launchMode: 'default',
-    scene: null
+    scene: null,
+    mode: ['examples']
   },
   {
     title: 'count-down-示例',
     page: 'examples/count-down/index',
     query: '',
     launchMode: 'default',
-    scene: null
+    scene: null,
+    mode: ['examples']
   },
   {
     title: 'range-date-picker-示例',
     page: 'examples/range-date-picker/index',
     query: '',
     launchMode: 'default',
-    scene: null
+    scene: null,
+    mode: ['examples']
   },
   {
     title: 'popup-示例',
     page: 'examples/popup/index',
     query: '',
     launchMode: 'default',
-    scene: null
+    scene: null,
+    mode: ['examples']
   },
   {
     title: 'toast-示例',
     page: 'examples/toast/index',
     query: '',
     launchMode: 'default',
-    scene: null
+    scene: null,
+    mode: ['examples']
   }
 ]
