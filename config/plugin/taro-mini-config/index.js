5b5a396a60c4076d3cdb9419ebb1d3868bcd2a7d
yangtianxia <1304082170@qq.com>
2023-11-24 11:16:08 +0800

    fix(plugin): 修复配置插件

--- config/plugin/taro-mini-config/index.js
+++ config/plugin/taro-mini-config/index.js
@@ -1,3 +1,4 @@
+const path = require('path')
 const shell = require('shelljs')
 const extend = require('extend')
 const fs = require('fs-extra')
@@ -11,7 +12,11 @@ const { miniConfigMap, envMap } = require('./utils')
 module.exports = (ctx, option = {}) => {
   const taroEnv = getCurrentTaroEnv()
 
-  if (isNil(taroEnv) || taroEnv === 'h5') return
+  if (
+    isNil(taroEnv) ||
+    taroEnv === 'h5' ||
+    !shell.test('-d', path.resolve(__dirname, taroEnv))
+  ) return
 
   const run = require(`./${taroEnv}`)
   const projectConfigName = Reflect.get(miniConfigMap, taroEnv)
@@ -20,43 +25,42 @@ module.exports = (ctx, option = {}) => {
   const sourceRoot = Reflect.get(option, 'sourceRoot')
   const globalOption = Reflect.get(option, 'global') || {}
 
-  async function copyConfig() {
-    const formRoot = resolve(sourceRoot, taroEnv)
+  ctx.onBuildFinish(() => {
+    if (shell.test('-e', outputPath)) {
+      const env = loadEnv()
+      const partial = Object
+        .keys(env)
+        .reduce(
+          (obj, key) => {
+            if (Reflect.has(envMap, key)) {
+              const newKey = Reflect.get(envMap, key)
+              const value = Reflect.get(env, key)
+              Reflect.set(obj, newKey, cleanEnvValue(value))
+            }
+            return obj
+          }, {}
+        )
 
-    if (shell.test('-d', formRoot)) {
-      await fs.copy(formRoot, ctx.paths.outputPath)
+      try {
+        const temp = shell.cat(outputPath)
+        const projectConfig = toJSON(temp)
+        const config = extend(true, partial, globalOption, dynamicOption)
+        const extraConfig = run(config)
+        const usedConfig = extend(true, projectConfig, extraConfig)
+        shell.ShellString(JSON.stringify(usedConfig)).to(outputPath)
+      } catch (err) {
+        console.log('❌', err)
+      }
     }
-  }
+  })
 
-  ctx.onBuildFinish(async () => {
-    // 先拷贝项目初始配置
-    await copyConfig()
+  ctx.onBuildComplete(async () => {
+    const fromRoot = resolve(sourceRoot, taroEnv)
 
-    if (!shell.test('-e', outputPath)) return
-
-    const env = loadEnv()
-    const partial = Object
-      .keys(env)
-      .reduce(
-        (obj, key) => {
-          if (Reflect.has(envMap, key)) {
-            const newKey = Reflect.get(envMap, key)
-            const value = Reflect.get(env, key)
-            Reflect.set(obj, newKey, cleanEnvValue(value))
-          }
-          return obj
-        }, {}
-      )
-
-    try {
-      const temp = shell.cat(outputPath)
-      const projectConfig = toJSON(temp)
-      const config = extend(true, partial, globalOption, dynamicOption)
-      const extraConfig = run?.(config)
-      const usedConfig = extend(true, projectConfig, extraConfig)
-      shell.ShellString(JSON.stringify(usedConfig)).to(outputPath)
-    } catch (err) {
-      console.log('❌', err)
+    if (shell.test('-d', fromRoot)) {
+      await fs.copy(fromRoot, ctx.paths.outputPath, {
+        overwrite: true
+      })
     }
   })
 }
