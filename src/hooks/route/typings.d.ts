b143615cecdd98b1bd3db7b6d052ff960c5b0f23
yangtianxia <1304082170@qq.com>
2023-11-29 11:55:35 +0800

    chore: 修复路由 validator 返回声明问题

--- src/hooks/route/typings.d.ts
+++ src/hooks/route/typings.d.ts
@@ -3,13 +3,13 @@ import router from '@/router'
 import { useRouter, getCurrentInstance } from '@tarojs/taro'
 import type { Ref } from 'vue'
 import type { USE_ROUTE_KEY } from '.'
-import type { ResultProps } from '@/components/result'
+import type { ResultStatusType } from '@/components/result'
 import type { Query } from '@/router/types'
 
-interface CurrentRoute extends ReturnType<typeof useRouter> {
+declare interface CurrentRoute extends ReturnType<typeof useRouter> {
   pageInstance?: ReturnType<typeof getCurrentInstance>,
   currentRoute: Ref<ReturnType<typeof router.getRoute>>,
-  activeUpdate(query?: Query): ResultProps | undefined
+  activeUpdate(query?: Query): ResultStatusType | undefined
 }
 
 declare module '@tarojs/taro' {
