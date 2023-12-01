fa43cc4622317eea16ea61afc5fde178e7b6cb54
yangtianxia <1304082170@qq.com>
2023-12-01 17:11:33 +0800

    chore: 优化Popup组件

--- src/components/popup/utils.ts
+++ src/components/popup/utils.ts
@@ -1,6 +1,6 @@
 import type { Interceptor } from '@txjs/shared'
-import type { PropType, CSSProperties, TeleportProps } from 'vue'
-import { truthProp, unknownProp, numericProp, makeNumericProp } from '../utils'
+import { inject, watch, type PropType, type CSSProperties, type TeleportProps } from 'vue'
+import { truthProp, unknownProp, numericProp, makeNumericProp, createInjectionKey } from '../utils'
 
 export const popupSharedProps = {
   show: Boolean,
@@ -24,3 +24,17 @@ export type PopupSharedPropKeys = Array<keyof typeof popupSharedProps>
 export const popupSharedPropKeys = Object.keys(
   popupSharedProps
 ) as PopupSharedPropKeys
+
+export const POPUP_TOGGLE_KEY = createInjectionKey<() => boolean>('popup-toggle')
+
+export const onPopupReopen = (callback: Callback) => {
+  const popupToggleStatus = inject(POPUP_TOGGLE_KEY, null)
+
+  if (popupToggleStatus) {
+    watch(popupToggleStatus, (show) => {
+      if (show) {
+        callback()
+      }
+    })
+  }
+}
